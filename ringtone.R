25d0ed186b36f0a87168498c7b8f2226c201e607
Paola Corrales <paobcorrales@gmail.com>
2024-07-22 20:29:15 +1000

    script

---
+++ ringtone.R
@@ -0,0 +1,24 @@
+# Create your own ringtone! 
+
+library(Rmusic) # Install it with remotes::install_github("keithmcnulty/Rmusic")
+
+seed <- 42   #choose a seed
+n_notes <- 5 #choose a integer between 1 and 5
+
+
+notes <- c("A", "B", "C", "D", "E", "F", "G")
+durations <- c(0.25, 0.33, 0.5, 0.75, 1, 1.5)
+semitones <- c("#", "b")
+
+# Don't change anything after this line
+set.seed(seed)
+
+song <- sample(notes, n_notes, replace = TRUE)
+song <- paste0(song, sample(semitones, 1))
+song <- c(song, paste0(song[n_notes], "b"))
+
+duration <- sample(durations, n_notes, replace = TRUE)
+duration <- c(duration, 2)
+
+# Play the ringtone!
+play_music(song, duration)
